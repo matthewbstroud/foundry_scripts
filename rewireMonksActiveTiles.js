57941bfef3f29b256803b18c8039fdb920d76bd6
Matthew Stroud <mbstroud@gmail.com>
2023-02-13 19:04:34 -0500

    add rewire script for monks active tiles

---
+++ rewireMonksActiveTiles.js
@@ -0,0 +1,7 @@
+canvas.scene.tiles.filter(t => t.data.flags["monks-active-tiles"].actions.length > 0).forEach(tile => {
+    var tileData = JSON.stringify(tile.data.flags['monks-active-tiles']);
+    tileData = tileData.replace(/Scene\.\w+/g, canvas.scene.uuid);
+    tile.update({
+        'flags.monks-active-tiles': JSON.parse(tileData)
+    });
+});
