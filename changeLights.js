4a9d0fec1aa15bf95dd68d7295bbc5b47e0d2c33
Matthew Stroud <mbstroud@gmail.com>
2023-02-14 20:02:25 -0500

    added a script to bulk update lights

---
+++ changeLights.js
@@ -0,0 +1,95 @@
+
+function updateLights(search_color, search_animation, new_color, new_animation, min_activation, max_activation) {
+    let targetLights = canvas.scene.lights
+        .filter(l => l.data.config.color == search_color && l.data.config.animation.type == search_animation);
+
+    targetLights.forEach(l => {
+        l.update({
+            "config.color": new_color,
+            "config.animation.type": new_animation,
+            "config.darkness.min": min_activation,
+            "config.darkness.max": max_activation
+        });
+    });
+    ui.notifications.notify(`Updated ${targetLights.length} lights.`);
+}
+
+function toNumber(val) {
+    if (!val || val == '') {
+        return 0;
+    }
+    return Number(val);
+}
+
+let lightsForm = `
+<b>Select Search Criteria:</b><br />
+<div style="display: flex; width: 100%; margin: 10px 0px 10px 0px">
+    <label for="search_color" style="white-space: nowrap; margin: 4px 10px 0px 10px;">Color:</label>
+    <input type="string" id="search_color" name="search_color" />
+    <label for="search_animation" style="white-space: nowrap; margin: 4px 10px 0px 10px;">Animation:</label>
+    <select name="search_animation" id="search_animation">
+        <option value="">None</option>
+        <option value="torch">Torch</option>
+    </select>
+</div>
+<b>Set New Values:</b><br />
+<div style="display: flex; width: 100%; margin: 10px 0px 10px 0px">
+    <label for="new_color" style="white-space: nowrap; margin: 4px 10px 0px 10px;">Color:</label>
+    <input type="string" id="new_color" name="new_color" />
+    <label for="new_animation" style="white-space: nowrap; margin: 4px 10px 0px 10px;">Animation:</label>
+    <select name="new_animation" id="new_animation">
+        <option value="">None</option>
+        <option value="torch">Torch</option>
+    </select>
+</div>
+<span>Darkness Activation Range</span><br />
+<div style="display: flex; width: 100%; margin: 10px 0px 10px 0px">
+    <label for="min_activation" style="white-space: nowrap; margin: 4px 10px 0px 10px;">Min:</label>
+    <input type="number" value="0" step="0.1" min="0" max="1" id="min_activation" name="min_activation" />
+    <label for="max_activation" style="white-space: nowrap; margin: 4px 10px 0px 10px;">Max:</label>
+    <input type="number" value="0" step="0.1" min="0" max="1" id="max_activation" name="max_activation" />
+</div>
+`;
+
+new Dialog({
+    title: `Update Lights`,
+    content: `
+        <form>
+            ${lightsForm}
+        </form>
+    `,
+    buttons: {
+        yes: {
+            icon: "<i class='fas fa-check'></i>",
+            label: `Update`,
+            callback: (html) => {
+                debugger;
+                let search_color = html.find('#search_color').val();
+                if (search_color.length == 0) {
+                    search_color = null;
+                }
+                let search_animation = html.find('#search_animation').val();
+                if (search_animation.length == 0) {
+                    search_animation = null;
+                }
+                let new_color = html.find('#new_color').val();
+                if (new_color.length == 0) {
+                    new_color = null;
+                }
+                let new_animation = html.find('#new_animation').val();
+                if (new_animation.length == 0) {
+                    new_animation = null;
+                }
+                let min_activation = toNumber(html.find('#min_activation').val());
+                let max_activation = toNumber(html.find('#max_activation').val());
+
+                updateLights(search_color, search_animation, new_color, new_animation, min_activation, max_activation);
+            }
+        },
+        no: {
+            icon: "<i class='fas fa-times'></i>",
+            label: `Cancel`
+        },
+    },
+    default: "yes"
+}).render(true)
