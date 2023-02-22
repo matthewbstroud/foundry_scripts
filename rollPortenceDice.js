4c59c5da3ce9e6823c7097218593a9cffe0a0b5c
Matthew Stroud <mbstroud@gmail.com>
2023-02-22 18:14:35 -0500

    Create macro to generate portence dice

---
+++ rollPortenceDice.js
@@ -0,0 +1,125 @@
+if (!actor) {
+    ui.notifications.notify('No current actor.');
+    return;
+}
+if (!actor.items.find(i => i.name.startsWith("Portent") && i.data.type === "feat")) {
+    ui.notifications.notify(`${actor.name} doesn't have the Portent ability.`);
+    return;
+}
+
+let existingDice = actor.items.filter(i => i.name.startsWith("Portent") && i.data.type === "consumable");
+let portent = actor.data.items.find((i) => i.name == "Portent" && i.data.type == "feat");
+let currentUses = parseInt(portent.data.data.uses.value);
+if (existingDice.length > 0 || currentUses < 2) {
+    ui.notifications.notify(`You have already rolled portent dice today.`);
+    return;
+}
+
+let dice1 = await createPortentDice(actor);
+let dice2 = await createPortentDice(actor);
+
+ChatMessage.create({ 
+    speaker: { alias: actor.name }, 
+    content: `Summons two mystical dice, each die has the same value on every face. The values are ${dice1} and ${dice2}.`, 
+    type: CONST.CHAT_MESSAGE_TYPES.EMOTE 
+});
+
+function createPortentDice(actor) {
+    let diceRoll = new Roll('1d20').evaluate({ async: false });
+    let portentDice =
+    {
+        "name": `Portent Dice (${diceRoll.total})`,
+        "type": "consumable",
+        "img": "icons/sundries/gaming/dice-runed-tan.webp",
+        "data": {
+            "description": {
+                "value": "A magical dice that can change the destiny of the possessor.",
+                "chat": "",
+                "unidentified": ""
+            },
+            "source": "",
+            "quantity": 1,
+            "weight": 0,
+            "price": 0,
+            "attunement": 0,
+            "equipped": false,
+            "rarity": "",
+            "identified": true,
+            "activation": {
+                "type": "bonus",
+                "cost": 1,
+                "condition": ""
+            },
+            "duration": {
+                "value": null,
+                "units": ""
+            },
+            "target": {
+                "value": null,
+                "width": null,
+                "units": "",
+                "type": ""
+            },
+            "range": {
+                "value": null,
+                "long": null,
+                "units": ""
+            },
+            "uses": {
+                "value": 1,
+                "max": "1",
+                "per": "charges",
+                "autoDestroy": true
+            },
+            "consume": {
+                "type": "",
+                "target": "",
+                "amount": null
+            },
+            "ability": "",
+            "actionType": "other",
+            "attackBonus": 0,
+            "chatFlavor": "",
+            "critical": {
+                "threshold": null,
+                "damage": ""
+            },
+            "damage": {
+                "parts": [
+                    [
+                        `${diceRoll.total}`,
+                        "midi-none"
+                    ]
+                ],
+                "versatile": ""
+            },
+            "formula": "",
+            "save": {
+                "ability": "",
+                "dc": null,
+                "scaling": "spell"
+            },
+            "consumableType": "trinket"
+        },
+        "effects": [],
+        "flags": {
+            "midi-qol": {
+                "effectActivation": false,
+                "onUseMacroName": "[postDamageRoll]syncPortentDice"
+            },
+            "midiProperties": {
+                "nodam": false,
+                "fulldam": false,
+                "halfdam": false,
+                "rollOther": false,
+                "critOther": false,
+                "magicdam": false,
+                "magiceffect": false,
+                "concentration": false,
+                "toggleEffect": false
+            }
+        }
+    };
+    actor.createEmbeddedDocuments('Item', [portentDice]);
+    return diceRoll.total;
+}
---
+++ syncPortentDice.js
@@ -0,0 +1,31 @@
+let actor = args[0].actor;
+
+
+
+if (!actor.data.flags.ddbimporter) {
+    return;
+}
+
+let actorResources = actor.data.flags.ddbimporter.resources;
+
+let diceResource = "";
+if (actorResources.primary = "Portent") {
+    diceResource = "primary";  
+}
+else if (actorResources.secondary = "Portent") {
+    diceResource = "secondary";  
+}
+else if (actorResources.tertiary = "Portent") {
+    diceResource = "tertiary";  
+}
+
+if (diceResource == "") {
+    return;
+}
+let portent = actor.data.items.find((i) => i.name == "Portent");
+let currentUses = parseInt(portent.data.data.uses.value) - 1;
+if (currentUses < 0) {
+    return;
+}
+portent.update({ "data.uses.value": currentUses});
+actor.update({ [`data.resources.${diceResource}.value`]: currentUses });
