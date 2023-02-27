f621ce3495a564d0240deada1f2479446f1feccd
Matthew Stroud <mbstroud@gmail.com>
2023-02-27 18:15:50 -0500

    combatMusic

---
+++ combatMusic.js
@@ -0,0 +1,22 @@
+/*
+combatMusic - play combat music starting with a random selection
+SyncUrl=https://raw.githubusercontent.com/matthewbstroud/foundry_scripts/main/combatMusic.js
+*/
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+let combatPlaylist = game.playlists.getName("Combat");
+if (combatPlaylist) {
+    if (combatPlaylist.playing) {
+        combatPlaylist.stopAll();
+        return;
+    }
+    let songs = combatPlaylist.playbackOrder;
+    let songIndex = getRandomInt(0, songs.length);
+    combatPlaylist.playNext(songs[songIndex]);
+    combatPlaylist.playAll();
+}
