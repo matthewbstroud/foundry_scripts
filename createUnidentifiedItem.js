a3a40847cdd0082a7c985827b39fa8a4e5518f9f
Matthew Stroud <mbstroud@gmail.com>
2023-03-06 18:35:59 -0500

    add simple way to create unidentified placeholders for items

---
+++ createUnidentifiedItem.js
@@ -0,0 +1,74 @@
+/*
+Create a placeholder item to give a user until they can identify it
+SyncUrl=https://raw.githubusercontent.com/matthewbstroud/foundry_scripts/main/createUnidentifiedItem.js
+*/
+let items = Object.values(ui.windows).filter(w => (w.object instanceof Item));
+
+if (!items || items.length == 0 || items.length > 1) {
+    ui.notifications.notify(`Must only have one item sheet open!`);
+    return;
+}
+
+let item = items[0].object;
+
+
+function createUnidentifiedItem(item, name, description) {
+    let newItem = {
+        "name": `Unidentified ${name}`,
+        "type": item.data.type,
+        "img": item.data.img,
+        "data": {
+            "description": {
+                "value": description,
+                "chat": "",
+                "unidentified": "Wondrous item"
+            },
+            "quantity": 1,
+            "weight": item.data.data.weight,
+            "attunement": 0,
+            "equipped": false,
+            "identified": false
+        }, 
+        "flags": {
+            "world": {
+                "identified_id": item.id
+            }
+        }
+    };
+    Item.create(newItem);
+}
+
+let currencyTotals = `
+<b>Source Item: ${item.name}</b><br />
+<div style="display: block; width: 100%; margin: 10px 0px 10px 0px">
+<label for="name">Name:</label>
+<input id="name" name="description" value="${item.name}"/><br/>
+<label for="description">Description:</label>
+<textarea id="description" name="description" rows="10" cols="50">${item.data.data.description.value}</textarea>
+</div>
+`;
+
+new Dialog({
+    title: `Create Unidentified Item`,
+    content: `
+        <form>
+            ${currencyTotals}
+        </form>
+    `,
+    buttons: {
+        yes: {
+            icon: "<i class='fas fa-check'></i>",
+            label: `Create`,
+            callback: (html) => {
+                let name = html.find('#name').val();
+                let desc = html.find('#description').val();
+                createUnidentifiedItem(item, name, desc);
+            }
+        },
+        no: {
+            icon: "<i class='fas fa-times'></i>",
+            label: `Cancel`
+        },
+    },
+    default: "yes"
+}).render(true)
---
+++ identifyItem.js
@@ -0,0 +1,81 @@
+/*
+Identify a single item in a player's inventory
+SyncUrl=https://raw.githubusercontent.com/matthewbstroud/foundry_scripts/main/identifyItem.js
+*/
+if (canvas.tokens.controlled.length != 1) {
+    ui.notifications.notify(`Please select a single actor!`);
+    return;
+}
+let targetActor = canvas.tokens.controlled[0].actor;
+if (!targetActor || targetActor.data.type == "npc") {
+    ui.notifications.notify(`Please select a player!`);
+    return;
+}
+let unidentifiedItems = canvas.tokens.controlled[0].actor.items.filter(i => i.getFlag("world", "identified_id"));
+
+if (!unidentifiedItems || unidentifiedItems.length == 0) {
+    ui.notifications.notify(`${targetActor.name} has no unidentified items.`);
+    return;
+}
+
+let itemOptions = '';
+unidentifiedItems.forEach(i => {
+    itemOptions += `<option value="${i.id}">${i.name}</option>`;
+});
+
+
+async function identifyItem(target, itemID) {
+    debugger;
+    let placeHolderItem = target.items.get(itemID);
+    if (!placeHolderItem) {
+        ui.notifications.notify(`Item no longer exists in player inventory!`);
+        return;
+    }
+    let actualItemID = placeHolderItem.getFlag("world", "identified_id");
+    if (!actualItemID) {
+        ui.notifications.notify(`Item doesn't specify a parent!`);
+        return;
+    }
+    let actualItem = game.items.get(actualItemID);
+    if (!actualItem){
+        return;
+    }
+    let actualItemData = actualItem.toObject();
+    actualItemData.data.identified = true;
+    await target.createEmbeddedDocuments("Item", [actualItemData]);
+    await placeHolderItem.delete();
+}
+
+let unidentifiedItemsForm = `
+<div style="display: block; width: 100%; margin: 10px 0px 10px 0px">
+<label for="name">Select Item:</label>
+<select name="item_to_identify" id="item_to_identify">
+        ${itemOptions}
+</select>
+</div>
+`;
+
+new Dialog({
+    title: `Identify Item`,
+    content: `
+        <form>
+            ${unidentifiedItemsForm}
+        </form>
+    `,
+    buttons: {
+        yes: {
+            icon: "<i class='fas fa-check'></i>",
+            label: `Identify`,
+            callback: (html) => {
+                let id = html.find('#item_to_identify').val();
+                console.log(id);
+                identifyItem(targetActor, id);
+            }
+        },
+        no: {
+            icon: "<i class='fas fa-times'></i>",
+            label: `Cancel`
+        },
+    },
+    default: "yes"
+}).render(true)
