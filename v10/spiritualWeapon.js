9434554559dc7e24f72342ca87cd2fbbe4959651
Matthew Stroud <mbstroud@gmail.com>
2023-04-22 10:23:55 -0400

    spiritual weapon

---
+++ v10/spiritualWeapon.js
@@ -0,0 +1,24 @@
+//SyncUrl=https://raw.githubusercontent.com/matthewbstroud/foundry_scripts/main/v10/spiritualWeapon.js
+//provided by siliconsaint for honeybadger's warpgate v1.5.0
+//https://github.com/trioderegion/warpgate
+
+
+const GM_MACRO = "spiritualWeapon_GM";
+
+let gmMacro = game.macros.getName(GM_MACRO);
+
+if (!gmMacro) {
+    ui.notifications.notify(GM_MACRO);
+    return;
+}
+
+let spiritualWeapon = `modules/JB2A_DnD5e/Library/2nd_Level/Spiritual_Weapon/SpiritualWeapon_Mace01_01_Spectral_Blue_200x200.webm`;
+let crosshairConfig = {
+    icon: spiritualWeapon,
+    label: "Place Weapon"
+};
+
+let crosshairData = await warpgate.crosshairs.show(crosshairConfig);
+
+
+gmMacro.execute(game.user.id, args[0].actor._id, args[0].tokenId, args[0].spellLevel, crosshairData.x, crosshairData.y);
