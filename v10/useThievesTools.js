5fc5566e9532e1399a18855be65c0767ad1adf84
Matthew Stroud <mbstroud@gmail.com>
2023-04-23 17:53:08 -0400

    update thieves tools macro

---
+++ v10/useThievesTools.js
@@ -0,0 +1,17 @@
+/*
+fast roll thieves tools
+SyncUrl=https://raw.githubusercontent.com/matthewbstroud/foundry_scripts/main/useThievesTools.js
+*/
+
+if (canvas.tokens.controlled.length != 1) {
+    ui.notifications.info("You must have a single token selected!");
+    return;
+}
+let currentActor = canvas.tokens.controlled[0].actor;
+let thievesTools = currentActor.items.getName("Thieves' Tools");
+
+if (!thievesTools) {
+    ui.notifications.error(`${currentActor.name} doesn't have Thieves' Tools!`);
+    return;
+}
+thievesTools.rollToolCheck({ "fastForward": true });
