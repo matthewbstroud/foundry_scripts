09a187b3650c6130ce82bae3da799c4280fcc1e1
Matthew Stroud <mbstroud@gmail.com>
2023-05-15 18:43:31 -0400

    Apply Adhoc Damage

---
+++ applyAdhocDamage.js
@@ -0,0 +1,92 @@
+/*
+applyAdhocDamage - used to quickly create damage of any type and amount and apply it to the selected players.'
+SyncUrl=https://raw.githubusercontent.com/matthewbstroud/foundry_scripts/main/applyAdhocDamage.js
+*/
+
+
+let adHocDamage = {
+    releaseInvalidTokens: function _releaseInvalidTokens() {
+        function shouldRelease(token) {
+            const excludedFolders = ["Traps", "Loot", "Summons"];
+            if (token.inCombat) {
+                return true;
+            }
+            var folderName = token?.actor?.folder?.name ?? "root";
+            if (excludedFolders.includes(folderName)) {
+                return true;
+            }
+            if (token.actor.effects.filter(e => e.label == "Dead").length > 0) {
+                return true;
+            }
+            if (token.actor.effects.filter(e => e.data.label == "Dead").length > 0) {
+                return true;
+            }
+            return false;
+        }
+        let tokensToRelease = canvas.tokens.controlled.filter(t => shouldRelease(t));
+        tokensToRelease.forEach(t => {
+            t.release();
+        });
+    },
+    createButtonDialog: async function _createButtonDialog(title, buttons, direction) {
+        let selected = await warpgate.buttonDialog(
+            {
+                buttons,
+                title
+            },
+            direction
+        );
+        return selected;
+    },
+    getDamageType: async function _getDamageType(){
+        let damageButtons = Object.entries(CONFIG.DND5E.damageTypes).map(([k, v], i) => ({label: v, value: k}));
+        return await this.createButtonDialog("Select Damage Type", damageButtons, 'column');
+    },
+    getDamageDice: async function _getDamageDice(){
+        let diceButtons = ['d4', 'd6', 'd8', 'd10', 'd20', 'd100'];
+        return await this.createButtonDialog("Select Damage Dcie", diceButtons.map(v => ({label: v, value: v})), 'column');
+    },
+    getDiceCount: async function _getDiceCount(){
+        let numberButtons = [];
+        for (let i = 1; i <= 20; i++){
+            numberButtons.push({label: `${i}`, value: i});
+        }
+        return await this.createButtonDialog("How Many Dice?", numberButtons, 'row');
+    },
+    getSortedNames: function _getSortedNames(targets){
+        let sortedNames = targets.map(t => t.name).sort();
+        let targetNames = sortedNames.slice(0, sortedNames.length - 1).join(`, `);
+        if (sortedNames.length > 1){
+            targetNames += ` and ${sortedNames[sortedNames.length-1]} have`;
+        }
+        else {
+            targetNames = `${sortedNames[0]} has`;
+        }
+        return targetNames;
+    }
+};
+
+// release tokens that shouldn't take damage
+adHocDamage.releaseInvalidTokens();
+// get the tokens remaining
+let targets = canvas.tokens.controlled;
+if (!targets || targets.length == 0){
+    ui.notifications.notify(`No valid tokens selected!`);
+    return;
+}
+
+let damageType = await adHocDamage.getDamageType();
+if (!damageType){
+    return;
+}
+let damageDice = await adHocDamage.getDamageDice();
+if (!damageDice){
+    return;
+}
+let diceCount = await adHocDamage.getDiceCount();
+if (!diceCount){
+    return;
+}
+const damageRoll = await new Roll(`${diceCount}${damageDice}[${damageType}]`).evaluate({ async: true })
+damageRoll.toMessage({flavor: `${adHocDamage.getSortedNames(targets)} been struck with ${CONFIG.DND5E.damageTypes[damageType]}!`});
+await MidiQOL.applyTokenDamage([{ type: `${damageType}`, damage: damageRoll.total }], damageRoll.total, new Set(targets), null, new Set(), {});
